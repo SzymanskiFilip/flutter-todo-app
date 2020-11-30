e351a0eb51f40ec2027214e49b4950ee6e410462
Filip Szymanski <szymanski.fp@gmail.com>
2020-11-30 22:40:11 +0100

    added a appbar and a dummy form text in the center

--- todo/lib/adding_screen.dart
+++ todo/lib/adding_screen.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class AddingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Add Your Todo!")),
+      ),
+      body: Center(
+        child: Text("FORM"),
+      ),
+    );
   }
 }
