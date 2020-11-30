8b0f4c9688df41daf335145ef5a5cadafac7b312
Filip Szymanski <szymanski.fp@gmail.com>
2020-11-30 22:51:13 +0100

    added a variable on submit and a validator to the form

--- todo/lib/adding_screen.dart
+++ todo/lib/adding_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AddingScreen extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,12 +11,19 @@ class AddingScreen extends StatelessWidget {
       ),
       body: Center(
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               Padding(
                 padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
                 child: TextFormField(
                   decoration: InputDecoration(hintText: 'Your new Todo!'),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a new Todo!';
+                    }
+                    return null;
+                  },
                 ),
               ),
             ],
